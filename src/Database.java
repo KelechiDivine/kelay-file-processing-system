d18a9e5d7792e0688b0ffee081328e3b20744d6e
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-02-09 18:40:53 +0100

    This database has three categories

--- src/Database.java
+++ src/Database.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Database {
+	
+	private ArrayList<Member> goldMembers;
+	private ArrayList<Member> platinumMembers;
+	private ArrayList<Member> silverMembers;
+	
+	{
+		goldMembers = new ArrayList<>();
+	}
+	
+	public ArrayList<Member> getGoldMembers() {
+		return goldMembers;
+	}
+	{
+		platinumMembers = new ArrayList<>();
+	}
+		public ArrayList<Member> getSilverMembers () {
+			return silverMembers;
+		
+	}
+	{
+		silverMembers = new ArrayList<>();
+	}
+	public ArrayList<Member> getPlatinumMembers() {
+		return platinumMembers;
+	}
 }
--- test/FileProcessingSystemTest.java
+++ test/FileProcessingSystemTest.java
@@ -2,12 +2,17 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FileProcessingSystemTest {
 	
+	FileProcessingSystem fps;
+	
 	@BeforeEach
 	void setUp() {
+		fps = new FileProcessingSystem();
 	}
 	
 	@AfterEach
@@ -17,10 +22,33 @@ class FileProcessingSystemTest {
 	@Test
 	void systemHasADatabase(){
 		//given
-		FileProcessingSystem fPS = new FileProcessingSystem();
 		//when
-		Database database = fPS.getDatabase();
+		Database database = fps.getDatabase();
 		//assert
 		assertNotNull(database);
+	}
+	
+	@Test
+	void databaseHasGoldMemberFilesCategory(){
+		//when
+		ArrayList<Member> gms = fps.getDatabase().getGoldMembers();
+		//assert
+		assertNotNull(gms);
+		assertEquals(0, gms.size());
+	}
+	@Test
+	void silverMemberFileShouldBeInDataBase(){
+		//when
+		ArrayList<Member> slm = fps.getDatabase().getSilverMembers();
+		//assert
+		assertNotNull(slm);
+		assertEquals(0,slm.size());
+	}
+	@Test
+	void platinumMemberFileShouldBeInDatabase(){
+		//when
+		ArrayList<Member> plm = fps.getDatabase().getPlatinumMembers();
+		assertNotNull(plm);
+		assertEquals(0, plm.size());
 	}
 }
